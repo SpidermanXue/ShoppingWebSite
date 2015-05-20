61445cac8a1c81c7b33b3c06e5b0efd1563905d1
Jian Xue <Evan@Jians-MacBook-Pro.local>
2015-05-19 18:36:00 -0700

    jian xue:

--- src/helpers/AnalyticsHelper.java
+++ src/helpers/AnalyticsHelper.java
@@ -11,12 +11,13 @@ public class AnalyticsHelper {
 	public static List<AnalyticsList> getList(String cate) throws Exception{
             Connection conn = HelperUtils.connect();
             Statement stmt = conn.createStatement();
-            List<AnalyticsList> analytics = new ArrayList<AnalyticsList>();
-            String sql = "CREATE TEMPORARY TABLE AS MyTempTable AS SELECT s.uid AS uid, u.name AS uname, s.pid AS pid, p.name AS pname, c.id AS cid, c.name AS cname, SUM(s.quantity) AS quan, s.price AS price, m.state AS state"+
+            List<AnalyticsList> analytics = new ArrayList<AnalyticsList>(); 
+            String sql = "CREATE TEMPORARY TABLE AS MyTempTable AS (SELECT s.uid AS uid, u.name AS uname, s.pid AS pid, p.name AS pname, c.id AS cid, c.name AS cname, SUM(s.quantity) AS quan, s.price AS price, m.state AS state"+
             "FROM (((categories c LEFT OUTER JOIN products p ON c.id=p.cid) LEFT OUTER JOIN sales s ON s.pid = p.id) LEFT OUTER JOIN users u ON s.uid = u.id) LEFT OUTER JOIN states m ON m.id = u.state" + 
-            		"Where c.name ='" + cate + "' GROUP BY s.uid, s.pid";
+            		"Where c.name ='" + cate + "' GROUP BY s.uid, s.pid) SELECT * FROM MyTempTable";
             //String sql2 = "CREATE TEMPORARY TABLE AS MyTempTabletwo AS SELECT * FROM (SELECT * FROM MyTempTable m GROUP BY uid) LEFT OUTER JOIN";
             ResultSet rs = stmt.executeQuery(sql);
+     
             while (rs.next()){
             	Integer uid = rs.getInt(1);
                 String uname = rs.getString(2);
