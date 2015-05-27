0b0b92aef95969422e0cc3c7fd90c932cbec0f68
Xin Wen <xiwen@ucsd.edu>
2015-05-26 22:47:02 -0700

    singup.js

---
+++ WebContent/js/signup.js
@@ -0,0 +1,25 @@
+function checkSignUpInfo(){
+	var name = document.getElementById("").value
+	var role = document.getElementById("").value
+	var age = document.getElementById("").value
+	var state = document.getElementById("").value
+	
+	if(name==null){
+		document.getElementById("").disabled = false
+	}
+	if(role==null){
+		document.getElementById("").disabled = false
+	}
+	if(name==null){
+		document.getElementById("").disabled = false
+	}
+	if(role==null){
+		document.getElementById("").disabled = false
+	}
+	
+	//user name taken
+	if(username taken){
+		document.getElementById("").disabled = false
+	}
+	
+}
